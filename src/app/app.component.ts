5e947529a2bf8b0dc99c579a892903a6750fdbd3
julienm <marque_julien@hotmail.fr>
2020-07-27 22:33:25 +0200

    error message added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'convertissor';
+  dec: number = 4;
+  bin: any = 0;
+
+  ngOnInit(){
+
+  }
+
+  binaryToDecimal(conv: any){
+    this.bin = conv;
+
+    const array = [];
+    let res = 0;
+
+    for( let i=conv.length-1; i>=0; i--){
+      if(conv[i] == '1'){
+        res = res + Math.pow(2, conv.length-i-1);
+      }
+    }
+    this.dec = res;
+  }
+
+  decimalToBinary(conv: number){
+    const array = [];
+
+    this.dec = conv;  
+
+    while(conv>=1){
+      array.unshift(conv%2);
+      conv = Math.floor(conv/2);
+    }
+    this.bin = array.join('');
+  }
+
+  onSubmit(f: NgForm){
+    console.log(f.value);
+
+    if(f.value.baseConv == 'Binaire'){
+      for(let i=0; i<f.value.conv.length; i++){
+        //Teste si le nombre binaire est valide
+      }
+      this.binaryToDecimal(f.value.conv);
+    }
+    else {
+      this.decimalToBinary(f.value.conv);
+    }
+  }
 }
